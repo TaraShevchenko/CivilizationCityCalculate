54d5f978252d88c9ce1ab78ccaedb7abb2b78da7
TaraShevchenko <tarasbizzz@gmail.com>
2021-04-13 20:53:22 +0300

    Versoin0.1
    First version

---
+++ test.js
@@ -0,0 +1,25 @@
+// const cities = [];
+//
+// function User(smile, cog, level, coins) {
+//     this.smile = smile;
+//     this.cog = cog;
+//     this.level = level;
+//     this.coins = coins;
+// }
+//
+// const onTest = document.querySelector('.test');
+// const onTestEdit = document.querySelector('.testEdit');
+//
+// onTest.addEventListener('click', function () {
+//     cities.push(new User(true, true, 1, 2))
+//     console.log(cities);
+// });
+//
+// onTestEdit.addEventListener('click', function () {
+//     cities[0].level = 3
+//     console.log(cities);
+// });
+
+
+
+
